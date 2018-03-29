d08f9e16b417607a5f1e235670a64f704d448a1e
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-03-29 13:46:02 +0100

    setting up redux actions and reducers

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,26 @@
+//increment
+export function increment(index){
+    return {
+        type: "INCREMENT_LIKES",
+        index
+    }
+}
+//add comment
+
+export function addCommnet(comment, author, postId){
+    return {
+        type: "ADD_COMMENT",
+        comment,
+        author,
+        postId
+    }
+}
+//delete comment
+
+export function deleteComment(postId, i){
+    return {
+        type: "DELETE_COMMENT",
+        postId,
+        i
+    }
+}
---
+++ client/reducers/posts.js
@@ -0,0 +1,11 @@
+//a reducer takes in two things
+
+//1. the actifon(info about what happended)
+//2. copy of current state
+
+function posts(state = [], action){
+    console.log(state, action);
+    return state;
+}
+
+export default posts;
